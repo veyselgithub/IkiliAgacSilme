00e785d820a9ab37434970c75633ec83e1846ba0
Veysel <veysel.dogan@ogr.sakarya.edu.tr>
2018-12-26 19:11:34 +0300

    Initial Commit

---
+++ IkiliAgacSilme/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IkiliAgacSilme
+//
+//  Created by Veysel on 26.12.2018.
+//  Copyright © 2018 Veysel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
